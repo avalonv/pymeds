e1b94cf8195a6d5896ce88c9436449a81e7b2540
avalonv <avalonvales@protonmail.com>
2019-05-02 11:20:50 -0300

    use clear_screen() and handle keyboard exceptions

--- pymed.py
+++ pymed.py
@@ -1,10 +1,9 @@
 from datetime import date, timedelta
 import json
 import time
+import os
 
 #TODO:
-# add 'total_taken' counter, 'first_taken' timestamp
-# implement info method
 
 """
 start program
@@ -13,11 +12,11 @@ start program
 #if there are no instances it
 
 +enter loop
-  #check len of Medication.instances. if it's zero add_medication() is called
+  #check len of Medication.instances. if it's zero add_med() is called
   #otherwise, call get_nextintake() on each of them
   #get_nextintake()
-  #each med is displayed next to a number, which will be the index in the Medicinies.instances list
-  #choice_action is what function/method will be called, followed by the index(es) of the instances+
+  #each med is displayed next to a number, which is its index in the Medicinies.instances list
+  #choice_action is what function/method will be called, followed by choice_num, the instance(s) it affects
   #actions:
 
     +mark meds that were taken
@@ -33,14 +32,13 @@ start program
       -go back to beggining of loop
 
     +add
-      #start add_medication()
+      #start add_med()
       -go back to beggining of loop
 
     +quit
       -breaks from the loop
 
 +serialize the instances as json objects and save them to my_file
-#it might be a good idea to save every time something changes instead
 
 end program
 
@@ -49,13 +47,22 @@ end program
 """
 
 # hardcoded for now:
-my_file = 'a.json'
-logging = True
+my_file = 'meds.json'
+logging = False
 
+# spits garbage
 # :g/debug_log/d
 def debug_log(*msg):
     if logging:
         print('log:', [item for item in msg])
+
+# :pray: https://stackoverflow.com/a/19596793/8225672
+def clear_screen():
+    if not logging:
+        os.system('clear') # clear output
+        #print('=======================================================')
+    else:
+        debug_log('clear_screen()')
 
 def time_now():
     return round(time.time())
@@ -81,52 +88,60 @@ def timestamp_to_date(my_timestamp):
 def increase_date(my_date, num):
     return my_date + timedelta(days=num)
 
-def strike(text):
-    # :pray: https://stackoverflow.com/a/25244576/8225672
+# :pray: https://stackoverflow.com/a/25244576/8225672
+def strikethrough(text):
     result = ''
     for c in text:
         result = result + c + '\u0336'
     return result
 
-def safe_cast(of_type, val, default=None, return_cast=True):
-    # :pray: https://stackoverflow.com/a/6330109/8225672
-    try:
-        cast_val = of_type(val)
-        if return_cast:
-            return cast_val
-        else:
-            return True
-    except (ValueError, TypeError):
+# :pray: https://stackoverflow.com/a/6330109/8225672
+# this function does way too much shit at once
+def safe_cast(of_type, val, default=None, rtn_cast=True):
+    if val != None: # for the love of god don't make None a string
+        try:
+            cast_val = of_type(val)
+            if rtn_cast:
+                return cast_val
+            else:
+                return True
+        except (ValueError, TypeError):
+            return default
+    else:
         return default
 
 def optional_ask(of_type, prompt):
     prompt += ': '
     while True:
-        ui = input(prompt)
-        if ui == '':
+        choice = input(prompt)
+        if choice.lower() == 'q':
+            if input('quit? ').lower() in 'y':
+                exit(0)
+        if choice == '':
             return None
-        #elif safe_cast(of_type, ui, default=False) == False:
         # if checking for an int and the user types '0' this condition will pass (which is not good)
+        #elif safe_cast(of_type, choice, default=False) == False:
         # check against None instead because (0 == False) = True; but (0 == None) = False
-        elif safe_cast(of_type, ui, return_cast=False) == None:
-            print('Wrong type')
+        elif safe_cast(of_type, choice, rtn_cast=False) == None:
+            print(f'(must be {str(of_type)})')
+             #type(of_type).__name__  or .__class__.__name__ doesn't work for some reason?
             continue
         else:
             break
-    return safe_cast(of_type, ui)
+    return safe_cast(of_type, choice)
 
 def required_ask(of_type, prompt):
-    prompt += '*'
+    prompt += ' *'
     while True:
-        ui = optional_ask(of_type, prompt)
-        if ui != None or '':
+        choice = optional_ask(of_type, prompt)
+        if choice != None or '':
             break
-        else: print("Can't be empty")
-    return safe_cast(of_type, ui)
+        else: print("this field can't be empty")
+    return safe_cast(of_type, choice)
 
 class Medication:
     instances = []
-    def __init__(self, name_generic, name_brand, dosage, doses_pc, cycle_len, notes, doses_taken=0, cycle_end=None, last_taken=None):
+    def __init__(self, name_generic, name_brand, dosage, doses_pc, cycle_len, notes=None, cycle_end=None, created_on=None, doses_taken=0, counter=0, last_taken=None):
 
         # append newly created instance
         #index=len(Medication.instances) # use a dict? idk tbh
@@ -140,101 +155,128 @@ class Medication:
         # string. ex: "estradot"
         self.name_brand = safe_cast(str, name_brand)
 
-        # the dosage for the medication
+        # dosage
         # string. ex: "100mg"
         self.dosage = safe_cast(str, dosage)
 
         # the number of doses to be taken per cycle
-        # int.    ex: "2"
+        # int.    ex: 1
         self.doses_pc = safe_cast(int, doses_pc)
 
-        # the number of hours between each cycle
+        # the number of days in each cycle
+        # (how long until the doses_taken counter is reset)
+        # int:   ex: 3
         self.cycle_len = safe_cast(int, cycle_len)
 
-        # any notes about the schedule or whatever
-        # str:   ex: "take 2 hours after eating"
+        # any notes about the medication
+        # str.   ex: "take 2 hours after eating"
         self.notes = safe_cast(str, notes)
 
         # the number of doses already taken
-        # int.    ex: "1"
+        # int.    ex: 1
         self.doses_taken = safe_cast(int, doses_taken)
 
         # timestamp of when the med was last taken
         # int. ex: 1556080265
         self.last_taken = safe_cast(int, last_taken)
 
-        # timestamp of when the current cycle begins
-        # int. ex:
+        # timestamp of when the current cycle ends and the counter is reset
+        # int. ex: 1556679600
         self.cycle_end = safe_cast(int, cycle_end)
         debug_log("type(self.cycle_end)", type(self.cycle_end))
 
+        # timestamp of when the medication was created
+        # int: ex: 1556766000
+        self.created_on = safe_cast(int, created_on)
+
+        # number of times the meds was taken
+        # int: ex: 30
+        self.counter = safe_cast(int, counter)
+
     def __str__(self):
+        string = f"{self.name_generic}"
         if self.name_brand != None:
-            return f"{self.name_brand} ({self.name_generic})"
-        return f"{self.name_generic}"
+            string = f"{self.name_brand} ({self.name_generic})"
+        if self.dosage != None:
+            string += f" {self.dosage}"
+        return string
 
     def take(self):
         self.last_taken = time_now()
         self.doses_taken += 1
+        self.counter += 1
 
     def untake(self):
         if self.doses_taken > 0:
             self.doses_taken -= 1
+
+        if self.counter > 0:
+            self.counter -= 1
 
     def get_lastintake(self):
         if self.last_taken != None:
             modifier = 's'
             base = seconds_passed(self.last_taken)
             if base > 60:
+                debug_log(f'get_lastintake base {base}s')
                 base = minutes_passed(self.last_taken)
                 modifier = 'm'
             if base > 60:
+                debug_log(f'get_lastintake base {base}m')
                 base = hours_passed(self.last_taken)
                 modifier = 'h'
             if base > 48:
+                debug_log(f'get_lastintake base {base}h')
                 base = days_passed(self.last_taken)
                 modifier = 'days'
             #return f'{base}{modifier}'
             return f'last taken {base}{modifier} ago'
         else:
+            debug_log(f'get_lastintake last_taken {self.last_taken}')
             return ''
+
+    def get_info(self):
+        infostr = f'{str(self)}\n'
+        if self.last_taken != None:
+            infostr+= f'{self.get_lastintake()} '
+        infostr += f'(counter resets on {timestamp_to_date(self.cycle_end)})'
+        if self.notes != None:
+            infostr += f'\nnotes: {self.notes}'
+        infostr += f'\ncounter: {self.counter}'
+        infostr += f'\nadded: {timestamp_to_date(self.created_on)}'
+        return infostr
+
 
     def get_dosesremaining(self):
         return f"{self.doses_taken}/{self.doses_pc}"
 
-    def _advance_next_cycle(self):
-        #date_ce = date.fromtimestamp(self.cycle_end)
-        date_ce = timestamp_to_date(self.cycle_end)
-        debug_log("_advance_next_cycle for", str(self), "starting date_ce", date_ce)
-
-        '''
-        # cycle_end = (cycle_end + cycle) until cycle_end + cycle > date_today
-        # doses_taken = 0
-        '''
-        #while (date_ce + timedelta(days=self.cycle_len)) < date.today():
-        while date_ce <= date.today():
-            #date_ce = date_ce + timedelta(days=self.cycle_len)
-            debug_log('_advance_next_cycle increasing date_ce', date_ce)
-            date_ce = increase_date(date_ce, self.cycle_len)
-        self.doses_taken = 0
-        debug_log('_advance_next_cycle updated date_ce is', date_ce)
-
-        #self.cycle_end = int(time.mktime(time.strptime(str(date_ce), '%Y-%m-%d')))
-        self.cycle_end = date_to_timestamp(date_ce)
-
     def _update(self):
         '''
-        # checks if the current cycle is in the past
-        # spiro last_taken 2019-04-20 and spiro cycle = 1
+        # checks if the cycle_end is in the past
+        # spiro last_taken 2019-04-20 and spiro cycle_end = 1
           # if spiro last_taken (20) + spiro cycle (1) less than date today (21)
             # spiro advance cycle
         '''
-        if date.fromtimestamp(self.last_taken) + timedelta(days=self.cycle_len) < date.today():
-            debug_log(f"{self}._update - 1")
-            self._advance_next_cycle()
-        elif self.cycle_end < time_now():
-            debug_log(f"{self}._update - 2 {self.cycle_end} < {time_now()}")
-            self._advance_next_cycle()
+        if self.cycle_end < time_now():
+            debug_log(f"{self}._update - {self.cycle_end} < {time_now()}")
+
+            date_ce = timestamp_to_date(self.cycle_end) # date cycle ends
+            debug_log("_update for", str(self), "starting date_ce", date_ce)
+
+            '''
+            # cycle_end = (cycle_end + cycle) until cycle_end + cycle > date_today
+            # doses_taken = 0
+            '''
+            #while (date_ce + timedelta(days=self.cycle_len)) < date.today():
+            while date_ce <= date.today():
+                #date_ce = date_ce + timedelta(days=self.cycle_len)
+                debug_log('_update increasing date_ce', date_ce)
+                date_ce = increase_date(date_ce, self.cycle_len)
+            self.doses_taken = 0
+            debug_log('_update updated date_ce is', date_ce)
+
+            #self.cycle_end = int(time.mktime(time.strptime(str(date_ce), '%Y-%m-%d')))
+            self.cycle_end = date_to_timestamp(date_ce)
 
     def check_nextintake(self):
         self._update()
@@ -243,30 +285,40 @@ class Medication:
         else:
             return False
 
+def list_meds():
+    i = 0
+    for med in Medication.instances:
+        if med.check_nextintake():
+            print(f"  {i} - [{med.get_dosesremaining()}] {med} {med.get_lastintake()}")
+        else:
+            print(f"✓ {i} -", strikethrough(f"[{med.get_dosesremaining()}] {med}"))
+        i += 1
 
-def add_medication():
+def add_med():
     while True:
-        name_generic = required_ask(str,'Med name')
-        name_brand = optional_ask(str, 'Brand name')
-        dosage = optional_ask(str, 'Dosage')
-        doses_pc = required_ask(int, 'Doses')
-        cycle_len = required_ask(int, 'Cycle (days)')
-        notes = optional_ask(str, 'Notes')
-        doses_taken = 0
-        last_taken = time_now()
-        cycle_end = increase_date(timestamp_to_date(time_now()), cycle_len) # cycle_ends = date today + cycle lenght
-        cycle_end = date_to_timestamp(cycle_end)
-        debug_log('add_medication new cycle_end', cycle_end)
+        clear_screen()
+        print("creating new medication")
 
-        new_med = Medication(name_generic, name_brand, dosage, doses_pc, cycle_len, notes, doses_taken, cycle_end, last_taken)
-        debug_log("add_medication created:", new_med)
+        name_generic = required_ask(str, 'Generic name')
+        name_brand   = optional_ask(str, 'Brand name')
+        dosage       = optional_ask(str, 'Dosage')
+        doses_pc     = required_ask(int, 'Take ... dose(s)')
+        cycle_len    = required_ask(int, 'Every ... day(s)')
+        notes        = optional_ask(str, 'Notes')
+        created_on   = time_now()
+        # cycle_ends = date today + cycle lenght
+        cycle_end    = increase_date(timestamp_to_date(created_on), cycle_len)
+        cycle_end    = date_to_timestamp(cycle_end)
 
-        if input('break? ') == 'q':
-            break
+        debug_log('add_med new cycle_end', cycle_end)
+        new_med = Medication(name_generic, name_brand, dosage, doses_pc, cycle_len, notes, cycle_end, created_on)
+        print('created', str(new_med))
+
+        if input('Add another? ').lower() == 'y':
+            continue
+        break
 
 def save_to_file():
-    # this should only be called ONCE each time the program runs
-
     save_data = []
     for med in Medication.instances:
         attribute_dict = vars(med)
@@ -299,7 +351,7 @@ def load_instances():
 
     except (FileNotFoundError):
         choice = input(f"'{my_file}' doesn't exist. create? ").lower()
-        if choice == 'y':
+        if choice in 'y':
             debug_log('load_instances FileNotFoundError create', my_file)
             open(my_file, 'w')
         else:
@@ -310,36 +362,26 @@ def load_instances():
 
 def loop():
     if len(Medication.instances) == 0:
-        add_medication()
-
-    def print_all():
-        i = 0
-        for med in Medication.instances:
-            if med.check_nextintake():
-                print(f"  {i} - {med} [{med.get_dosesremaining()}] {med.get_lastintake()}")
-            else:
-                print(f"✓ {i} -", strike(f"{med} [{med.get_dosesremaining()}]"))
-            i += 1
-
+        add_med()
     while True:
-        print_all()
+        clear_screen()
+        list_meds()
         choice = input("[A]dd new, [R]emove, [T]ake, [U]ntake, [I]nfo, [Q]uit: ").lower()
         try:
-            # first char in index that can't be an int or space
+            # first char in index that isn't an int or space
             action_choice = [char for char in choice.split(' ') if char != '' and not safe_cast(int, char)][0]
             debug_log('action_choice', action_choice)
-        except (IndexError):
-            # user typed nothing
+        except (IndexError): # user typed nothing
             continue
         # all chars that can be an int (return cast = False so 0 passes the condition)
-        num_choice = [safe_cast(int, char) for char in choice if safe_cast(int, char, return_cast=False)]
+        num_choice = [safe_cast(int, char) for char in choice if safe_cast(int, char, rtn_cast=False)]
         debug_log('num_choice', num_choice)
-
-        if action_choice == 'a': # add
-            add_medication()
 
         if action_choice == 'q': # quit
             break
+
+        if action_choice == 'a': # add
+            add_med()
 
         if action_choice == 'r': # remove
             print(num_choice)
@@ -352,7 +394,7 @@ def loop():
                     continue
                 print(f'Delete {selected}?', end=': ')
                 if input().lower() in 'y':
-                    Medication.instances.remove(index)
+                    Medication.instances.remove(selected)
                 else:
                     continue
 
@@ -375,6 +417,22 @@ def loop():
                     continue
                 selected.untake()
 
-load_instances()
-loop()
-save_to_file()
+        if action_choice == 'i': # info
+            clear_screen()
+            for index in num_choice:
+                # catch index error
+                try:
+                    selected = Medication.instances[index]
+                    print(f'{index}: {selected.get_info()}')
+                except (IndexError):
+                    continue
+            input('')
+
+        save_to_file()
+
+try:
+    load_instances()
+    loop()
+except (KeyboardInterrupt):
+    print('')
+    exit(1)
