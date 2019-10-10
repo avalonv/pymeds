77fd5a5ad3e3772fa615f0b25e1334e81568c1a5
avalonv <avalonvales@protonmail.com>
2019-10-10 01:10:00 -0300

    update usage

--- pymeds.py
+++ pymeds.py
@@ -25,7 +25,7 @@ checkmark = '\u2713'  # checkmark symbol. '\u2611' and '\u2714' are cool too
 def usage():
     print('''usage:
     [C]ommand followed by one or more meds, which are represented by numbers.
-    Ex: `t 0 1 q` will mark meds with numbers zero and one as [t]aken and quit.
+    Ex: 't 0 1 q' will mark meds with numbers zero and one as [t]aken and quit.
     '*' can be used to select all meds at once.
     A check mark is displayed when all doses have been taken for the day,
     otherwise the time since the most recent dose was taken is shown.
